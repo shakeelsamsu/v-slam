9a7dbf79e53edb4236d9777e306e8a0362410cef
Kapil <kapilrampalli@utexas.edu>
2022-05-08 15:41:19 -0500

    deep

--- ros_feature_matcher/src/ros_feature_matcher.cpp
+++ ros_feature_matcher/src/ros_feature_matcher.cpp
@@ -1,108 +1,133 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "nav_msgs/Odometry.h"
+#include "sensor_msgs/CompressedImage.h"
 #include <iostream>
-#include <dirent.h>
+#include <math.h>
 #include "opencv2/core.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/features2d.hpp"
-// #include "opencv2/xfeatures2d.hpp"
+#include <opencv2/imgcodecs.hpp>
+#include<opencv2/opencv.hpp>
+ #include <image_transport/image_transport.h>
+ #include <cv_bridge/cv_bridge.h>
+ using namespace std;
 
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc.hpp>
+/* Camera Calibration Stuff: 
+Opened Kinect with serial 000440615312
+Color camera: 
+type = 4
+cx = 641.786133
+cy = 366.616272
+fx = 608.228882
+fy = 608.156616
+k1 = 0.411396
+k2 = -2.710792
+k3 = 1.644717
+k4 = 0.291122
+k5 = -2.525489
+k6 = 1.563623
+codx = 0.000000
+cody = 0.000000
+p2 = 0.000494
+p1 = 0.000089
+metric_radius = 0.000000
+rotation = [
+ 0.999956  0.009388  0.000255 ;
+-0.009373  0.995875  0.090252 ;
+ 0.000593 -0.090251  0.995919 
+];
+translation = [
+-32.135025
+-2.009181
+ 3.814234
 
-using namespace cv;
-// using namespace cv::xfeatures2d;
-using namespace std;
+];
 
-const char* keys =
-    "{ help h |                  | Print help message. }"
-    "{ input1 | box.png          | Path to input image 1. }"
-    "{ input2 | box_in_scene.png | Path to input image 2. }"
-    "{ v verbose |               | Use of verbose flag. }";
+Depth camera: 
+type = 4
+cx = 163.385284
+cy = 157.269714
+fx = 252.774521
+fy = 252.861252
+k1 = 0.291972
+k2 = -0.109157
+k3 = -0.005082
+k4 = 0.633168
+k5 = -0.085589
+k6 = -0.030972
+codx = 0.000000
+cody = 0.000000
+p2 = -0.000053
+p1 = 0.000118
+metric_radius = 0.000000
+rotation = [
+ 1.000000  0.000000  0.000000 ;
+ 0.000000  1.000000  0.000000 ;
+ 0.000000  0.000000  1.000000 
+];
+translation = [
+ 0.000000
+ 0.000000
+ 0.000000
 
-const char* dataset_path = "../dataset/sequences/00/image_0/";
+];
 
-bool verbose = false;
-
-Mat readFromDataset(int num) {
-    string filename = to_string(num);
-    filename.insert(0, 6 - min(6, (int) filename.size()), '0');
-    string path(dataset_path);
-    filename.append(".png");
-    path.append(filename);
-    if(verbose) cout << path << endl;
-    return imread(path/*, IMREAD_GRAYSCALE */);
-}
-
-void twoFrameDetectAndCompute(Mat& img1, Mat& img2, Ptr<ORB> detector, vector<KeyPoint>& keypoints1, vector<KeyPoint>& keypoints2, Mat& descriptors1, Mat& descriptors2) {
-    detector->detectAndCompute( img1, noArray(), keypoints1, descriptors1 );
-    detector->detectAndCompute( img2, noArray(), keypoints2, descriptors2 );
-}
-
-std::vector<DMatch> match(Ptr<DescriptorMatcher> matcher, Mat& descriptors1, Mat& descriptors2) {
-    std::vector< std::vector<DMatch> > knn_matches;
-    matcher->knnMatch( descriptors1, descriptors2, knn_matches, 2 );
-    //-- Filter matches using the Lowe's ratio test
-    const float ratio_thresh = 0.7f;
-    std::vector<DMatch> good_matches;
-    for (size_t i = 0; i < knn_matches.size(); i++)
-    {
-        if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
-        {
-            good_matches.push_back(knn_matches[i][0]);
-        }
-    }
-    return good_matches;
-}
-
-void drawMatches(Mat& img1, Mat& img2, vector<KeyPoint>& keypoints1, vector<KeyPoint>& keypoints2, std::vector<DMatch> good_matches) {
-    Mat img_matches;
-    drawMatches( img1, keypoints1, img2, keypoints2, good_matches, img_matches, Scalar::all(-1),
-                 Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-    //-- Show detected matches
-    imshow("Good Matches", img_matches );
-    waitKey();
-}
-
-int main( int argc, char* argv[] )
+*/
+float odomX, odomY, odomTheta;
+void odomCallback(const nav_msgs::Odometry& msg)
 {
-    CommandLineParser parser( argc, argv, keys );
-    verbose = parser.get<bool>("verbose");
+    odomX = msg.pose.pose.position.x;
+    odomY = msg.pose.pose.position.y;
+    odomTheta = 2.0 * atan2(msg.pose.pose.orientation.z, msg.pose.pose.orientation.w);
+}
 
+bool savedRGB = false;
+void rgbCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+    cv_bridge::CvImagePtr img = cv_bridge::toCvCopy(msg);
+    // cout << "Got an rgb image!" << endl;
+}
 
-    for(int i = 0; i < 1000; i++) {
-        // read images
-        Mat img1 = readFromDataset(i);
-        Mat img2 = readFromDataset(i + 1);
-
-        if ( img1.empty() || img2.empty() )
-        {
-            cout << "Could not open or find the image!\n" << endl;
-            parser.printMessage();
-            return -1;
-        }
-
-        // get feature points and compute descriptors
-        int minHessian = 400;
-        Ptr<ORB> detector = ORB::create( minHessian );
-        std::vector<KeyPoint> keypoints1, keypoints2;
-        Mat descriptors1, descriptors2;
-        twoFrameDetectAndCompute(img1, img2, detector, keypoints1, keypoints2, descriptors1, descriptors2);
-
-        // match
-        Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
-        std::vector<DMatch> good_matches = match(matcher, descriptors1, descriptors2);
-
-        // draw some lines
-        for (int i = 0; i < (int) good_matches.size(); i++) {
-            if (verbose) 
-                cout << keypoints1[good_matches[i].queryIdx].pt << " " << keypoints2[good_matches[i].trainIdx].pt << endl;
-            line(img2, keypoints1[good_matches[i].queryIdx].pt, keypoints2[good_matches[i].trainIdx].pt, Scalar(0, 255, 0),
-                2, LINE_AA);
-        }
-        imshow("Lines", img2);
-        waitKey(10);
-        // imwrite("result.png", img2);
-
+bool savedDepth = false;
+cv::Mat depth(720, 1280, CV_16UC1);
+void depthCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+    try
+    {
+        memcpy(depth.data, msg->data.data(), msg->data.size());
+        cv::imshow("depth.jpg", depth);
+        cv::waitKey(10);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
     }
 
+    // cv::Mat mono8_img = cv::Mat(cv_ptr->image.size(), CV_8UC1);
+    // cv::convertScaleAbs(cv_ptr->image, mono8_img, 100, 0.0);
+    
+    // cout << "Got a depth image!" << endl;
+}
+
+void get3DPoint(int rgbX, int rgbY){
+    uint16_t pixelDepthMM = depth.at<uint16_t>(rgbX, rgbY);
+    uint32_t depthMeters = pixelDepthMM / 1000;
+    float normalizedX = ((float) rgbX / 1280) * 2 - 1;
+    float normalizedY = ((float) rgbY / 720) * 2 - 1;
+    cv::Vec3f dir{.2528, normalizedX, normalizedY};
+    // dir /= dir.norm();
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "ros_feature_matcher");
+    ros::NodeHandle n;
+    ros::Subscriber odomSub = n.subscribe("odom", 1000, odomCallback);
+    image_transport::ImageTransport it(n);
+    // ros::Subscriber rgbSub = n.subscribe("camera/rgb/image_raw/compressed", 1000, rgbCallback);
+    // ros::Subscriber depthSub = n.subscribe("camera/depth/image_raw/compressed", 1000, depthCallback);
+    image_transport::Subscriber rgbSub = it.subscribe("camera/rgb/image_raw", 1000, rgbCallback, image_transport::TransportHints("compressed"));
+    image_transport::Subscriber depthSub = it.subscribe("camera/depth/image_raw", 1000, depthCallback);
+    ros::spin();
     return 0;
 }
